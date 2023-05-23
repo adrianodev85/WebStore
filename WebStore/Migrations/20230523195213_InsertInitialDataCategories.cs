24a33244d3e8a23437b00ad81dac1ecedd883a38
Adriano Pinho <adrianopinho.dev@gmail.com>
2023-05-23 19:03:30 -0300

    Insert Inittial Data

---
+++ WebStore/Migrations/20230523195213_InsertInitialDataCategories.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertInitialDataCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(CategNome)" + "VALUES('Computing')");
+            migrationBuilder.Sql("INSERT INTO Categorias(CategNome)" + "VALUES('Games')");
+            migrationBuilder.Sql("INSERT INTO Categorias(CategNome)" + "VALUES('Telephony')");
+            migrationBuilder.Sql("INSERT INTO Categorias(CategNome)" + "VALUES('Software')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
---
+++ WebStore/Migrations/20230523200233_InsertInitialDataProducts.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertInitialDataProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Produtos(Descricao,DescricaoDetalhada,ValorUnitario,CategoriaId,ImgUrl,ImgThumbnailUrl,Preferido,EmEstoque)"
+                + "VALUES('X-Box Serie X','X-Box Serie X, 1TB SSD, 2 Wireless Controls, 16GB GDDR6 RAM, 8k HDR',4499.00,2,'~/wwwroot/img/xboxseriex.jpg','~/wwwroot/img/thumbxboxseriex.jpg',1,1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Produtos");
+        }
+    }
+}
